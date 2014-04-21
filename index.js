7e29a5210d8635cc40fbd14aa67a50d445010db1
GamingCoder <coreoder@gmail.com>
2014-04-21 16:52:49 +0200

    Program now works for logs without custom fields

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var util = require('util');
+
+function name (level) {
+	var levels = {"10":"trace","20":"debug","30":"info","40":"warn","50":"error","60":"fatal"};
+	return levels[level];
+}
+
 exports = module.exports = function(log) {
-	var cLog = '';
-	
+	var cLog = util.format('%s[%s/%d]: %s: msg=%s', log.name, log.hostname, log.pid, name(log.level), log.msg);
+
 	return cLog;
 };
