ab3286ca27019cb3c6789b04e6af4dccf4e784cd
GamingCoder <coreoder@gmail.com>
2014-07-24 13:48:36 +0200

    Fix typo and fix json detection in executable

--- bin/bunyan-readable.js
+++ bin/bunyan-readable.js
@@ -4,9 +4,9 @@ var readableBunyan = require('../index.js');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(data) {
-	if (data[0] === '{' && data[data.length-1] === '{') {
+	if (data[0] === '{') {
 		console.log(readableBunyan(JSON.parse(data)));
 	} else{
-		console.log data
+		console.log(data);
 	}
 });
