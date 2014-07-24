3fe0fdacb8a812061b890b44f1d461788e8ff7d8
GamingCoder <coreoder@gmail.com>
2014-07-24 13:43:48 +0200

    Handle non-bunyan messages in bin file

--- bin/bunyan-readable.js
+++ bin/bunyan-readable.js
@@ -4,11 +4,9 @@ var readableBunyan = require('../index.js');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(data) {
-	console.log(
-		readableBunyan(
-			JSON.parse(
-				data
-			)
-		)
-	);
+	if (data[0] === '{' && data[data.length-1] === '{') {
+		console.log(readableBunyan(JSON.parse(data)));
+	} else{
+		console.log data
+	}
 });
